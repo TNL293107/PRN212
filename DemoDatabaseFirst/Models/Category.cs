81bd41acbe07d0cfcd87f96d6975867df383d375
Long Nhat Tran <long293107@gmail.com>
2026-06-12 14:41:05 +0700

    Submit Code First and Database First

---
+++ DemoDatabaseFirst/Models/Category.cs
@@ -0,0 +1,15 @@
+namespace DemoDatabaseFirst.Models
+{
+    public partial class Category
+    {
+        public Category()
+        {
+            Products = new HashSet<Product>();
+        }
+
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}
---
+++ DemoDatabaseFirst/Models/MyStoreContext.cs
@@ -0,0 +1,57 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoDatabaseFirst.Models
+{
+    public partial class MyStoreContext : DbContext
+    {
+        public MyStoreContext() { }
+
+        public MyStoreContext(DbContextOptions<MyStoreContext> options) : base(options) { }
+
+        public virtual DbSet<Category> Categories { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("server=(local);database=MyStore;uid=sa;pwd=123;TrustServerCertificate=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Category>(entity =>
+            {
+                entity.Property(e => e.CategoryId).HasColumnName("CategoryID");
+                entity.Property(e => e.CategoryName)
+                    .IsRequired()
+                    .HasMaxLength(15)
+                    .UseCollation("Vietnamese_CI_AS");
+            });
+
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.Property(e => e.ProductId).HasColumnName("ProductID");
+                entity.Property(e => e.CategoryId).HasColumnName("CategoryID");
+                entity.Property(e => e.ProductName)
+                    .IsRequired()
+                    .HasMaxLength(40)
+                    .UseCollation("Vietnamese_CI_AS");
+                entity.Property(e => e.UnitPrice).HasColumnType("money");
+
+                entity.HasOne(d => d.Category)
+                    .WithMany(p => p.Products)
+                    .HasForeignKey(d => d.CategoryId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Products_Categories");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
